bf5f14036145dc8e06095e9195131f23cb251a0a
lorne <lorne.shi@gmail.com>
2022-10-11 14:18:33 +0800

    从https://github.com/Dragon-S/google-webrtc.git现在owt对应的webrtc

--- scripts/fetch_webrtc.sh
+++ scripts/fetch_webrtc.sh
@@ -8,6 +8,7 @@ FETCH_TARGET=`cat ${CONFIG_DIR}/webrtc_fetch`
 
 mkdir -p $WEBRTC_DIR
 cd $WEBRTC_DIR
+
 if [ -f $WEBRTC_DIR/.gclient ]; then
   echo "Syncing webrtc ...";
   cd $WEBRTC_DIR/src;
@@ -24,11 +25,26 @@ if [ -f $WEBRTC_DIR/.gclient ]; then
     git clean -xdf;
   fi
 else
-  echo "Getting WEBRTC ...";
+  echo "Create .gclient file ...";
   rm -f $DEPOT_TOOLS_DIR/metrics.cfg;
   rm -rf $WEBRTC_DIR/src;
-  fetch --nohooks $FETCH_TARGET;
+  gclient root
+  gclient config --spec 'solutions = [
+    {
+      "name": "src",
+      "url": "https://github.com/Dragon-S/google-webrtc.git",
+      "deps_file": "DEPS",
+      "managed": False,
+      "custom_deps": {},
+    },
+  ]
+  '
+
+  echo "Getting WEBRTC ...";
+  gclient sync --nohooks --with_branch_heads
 fi
+
+echo "Switch WEBRTC to WEBRTC_COMMIT = $WEBRTC_COMMIT";
 cd $WEBRTC_DIR/src
 git fetch
 git checkout -f $WEBRTC_COMMIT
